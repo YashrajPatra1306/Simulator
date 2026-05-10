ae11fa2fa0658c1b2575093fd92caf6b0b006cf1
Yashraj Priyadarshan Patra <yashrajpriyadarshanpatra16220@gmail.com>
2026-05-10 17:50:53 +0530

    Add new P/Invoke functions and update existing ones

--- sandbox-engine/cs-frontend/PhysicsInterop.cs
+++ sandbox-engine/cs-frontend/PhysicsInterop.cs
@@ -11,6 +11,9 @@ namespace SandboxEngine
         public static extern void init_physics(int width, int height);
 
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void resize_physics(int width, int height);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
         public static extern void update_physics(float dt, float mouseX, float mouseY, int width, int height);
 
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
@@ -20,7 +23,16 @@ namespace SandboxEngine
         public static extern void remove_object(int index);
 
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void set_object_position(int index, float x, float y);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
         public static extern void spawn_particles(float x, float y, int count);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wake_objects_near(float x, float y, float radius);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void set_gravity(float g);
 
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
         public static extern int get_object_count();
@@ -29,12 +41,11 @@ namespace SandboxEngine
         public static extern int get_particle_active_count();
 
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int get_max_objects();
+        public static extern int get_object_slot_count();
 
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void set_gravity(float g);
+        public static extern int get_particle_high_water_mark();
 
-        // Fixed: Out parameters instead of tuple
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
         public static extern void get_black_hole_position(out float x, out float y);
 
@@ -44,5 +55,30 @@ namespace SandboxEngine
         [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
         [return: MarshalAs(UnmanagedType.I1)]
         public static extern bool get_black_hole_active();
+
+        // Priority 2: One P/Invoke bulk copy for all render data per frame
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int copy_render_instances(IntPtr outBuf, int bufLen);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int copy_active_particles(IntPtr outBuf, int bufLen);
+
+        // Packed render struct: 20 bytes, GPU-friendly (x, y, radius, color, shape)
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct RenderInstance
+        {
+            public float X, Y, Radius;
+            public uint  Color;
+            public int   Shape;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct Particle
+        {
+            public float X, Y, VX, VY, Life, MaxLife;
+            public uint  Color;
+            public float Size;
+            [MarshalAs(UnmanagedType.I1)] public bool Active;
+        }
     }
 }
